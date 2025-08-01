1cdfe1569981e1b2363817cd79879cf08ff9e8f5
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-08-01 00:17:40 -0300

    feat: ponder event for profile contract

--- packages/ponder/ponder.schema.ts
+++ packages/ponder/ponder.schema.ts
@@ -2,75 +2,101 @@ import { onchainTable, primaryKey } from "ponder";
 
 // JobPosting table
 export const jobPosting = onchainTable("jobPosting", (t) => ({
-  postingId: t.bigint().primaryKey(),
-  freelancer: t.hex().notNull(),
-  basePayment: t.bigint().notNull(),
-  title: t.text().notNull(),
-  description: t.text().notNull(),
-  category: t.text().notNull(),
-  bannerImageUrl: t.text().notNull(), // Optional, can be null if not provided
-  minimumNoticeTime: t.bigint().notNull(),
-  averageWorkDuration: t.bigint().notNull(),
-  createdAt: t.bigint().notNull(),
+	postingId: t.bigint().primaryKey(),
+	freelancer: t.hex().notNull(),
+	basePayment: t.bigint().notNull(),
+	title: t.text().notNull(),
+	description: t.text().notNull(),
+	category: t.text().notNull(),
+	bannerImageUrl: t.text().notNull(), // Optional, can be null if not provided
+	minimumNoticeTime: t.bigint().notNull(),
+	averageWorkDuration: t.bigint().notNull(),
+	createdAt: t.bigint().notNull(),
 }));
 
 // Job table
-export const job = onchainTable("job", (t) => ({
-  jobId: t.bigint().notNull(),
-  postingId: t.bigint().notNull(),
-  client: t.hex().notNull(),
-  freelancer: t.hex().notNull(),
-  payment: t.bigint().notNull(),
-  title: t.text().notNull(),
-  description: t.text().notNull(),
-  category: t.text().notNull(),
-  bannerImageUrl: t.text().notNull(),
-  jobDuration: t.bigint().notNull(),
-  deadline: t.bigint(),
-  state: t.integer().notNull(),
-  createdAt: t.bigint().notNull(),
-  acceptedAt: t.bigint(),
-  clientReceived: t.boolean().notNull(),
-  freelancerDelivered: t.boolean().notNull(),
-}),
- (table) => ({
-  pk: primaryKey({ columns: [table.jobId, table.postingId] }),
-}));
+export const job = onchainTable(
+	"job",
+	(t) => ({
+		jobId: t.bigint().notNull(),
+		postingId: t.bigint().notNull(),
+		client: t.hex().notNull(),
+		freelancer: t.hex().notNull(),
+		payment: t.bigint().notNull(),
+		title: t.text().notNull(),
+		description: t.text().notNull(),
+		category: t.text().notNull(),
+		bannerImageUrl: t.text().notNull(),
+		jobDuration: t.bigint().notNull(),
+		deadline: t.bigint(),
+		state: t.integer().notNull(),
+		createdAt: t.bigint().notNull(),
+		acceptedAt: t.bigint(),
+		clientReceived: t.boolean().notNull(),
+		freelancerDelivered: t.boolean().notNull(),
+	}),
+	(table) => ({
+		pk: primaryKey({ columns: [table.jobId, table.postingId] }),
+	})
+);
 
 // Gigs table
 export const gig = onchainTable("gig", (t) => ({
-  gigId: t.bigint().primaryKey(),
-  client: t.hex().notNull(),
-  acceptedFreelancer: t.hex(),
-  maxPayment: t.bigint().notNull(),
-  finalPayment: t.bigint(),
-  title: t.text().notNull(),
-  description: t.text().notNull(),
-  category: t.text().notNull(),
-  maxDurationInHours: t.bigint().notNull(),
-  finalDurationInHours: t.bigint(),
-  deadline: t.bigint(),
-  state: t.integer().notNull(),
-  createdAt: t.bigint().notNull(),
-  acceptedAt: t.bigint(),
-  clientReceived: t.boolean().notNull(),
-  freelancerDelivered: t.boolean().notNull(),
-  acceptedApplicationId: t.bigint(),
+	gigId: t.bigint().primaryKey(),
+	client: t.hex().notNull(),
+	acceptedFreelancer: t.hex(),
+	maxPayment: t.bigint().notNull(),
+	finalPayment: t.bigint(),
+	title: t.text().notNull(),
+	description: t.text().notNull(),
+	category: t.text().notNull(),
+	maxDurationInHours: t.bigint().notNull(),
+	finalDurationInHours: t.bigint(),
+	deadline: t.bigint(),
+	state: t.integer().notNull(),
+	createdAt: t.bigint().notNull(),
+	acceptedAt: t.bigint(),
+	clientReceived: t.boolean().notNull(),
+	freelancerDelivered: t.boolean().notNull(),
+	acceptedApplicationId: t.bigint(),
 }));
 
 // Gig Applications table
-export const gigApplication = onchainTable("gigApplication", (t) => ({
-  applicationId: t.bigint().notNull(),
-  gigId: t.bigint().notNull(),
-  freelancer: t.hex().notNull(),
-  proposedPayment: t.bigint().notNull(),
-  proposedDurationInHours: t.bigint().notNull(),
-  state: t.integer().notNull(),
-  createdAt: t.bigint().notNull(),
-  proposalComment: t.text().notNull(),
-  rejectionComment: t.text(),
-}),
-(table) => ({
-  pk: primaryKey({ columns: [table.applicationId, table.gigId] }),
-}));
+export const gigApplication = onchainTable(
+	"gigApplication",
+	(t) => ({
+		applicationId: t.bigint().notNull(),
+		gigId: t.bigint().notNull(),
+		freelancer: t.hex().notNull(),
+		proposedPayment: t.bigint().notNull(),
+		proposedDurationInHours: t.bigint().notNull(),
+		state: t.integer().notNull(),
+		createdAt: t.bigint().notNull(),
+		proposalComment: t.text().notNull(),
+		rejectionComment: t.text(),
+	}),
+	(table) => ({
+		pk: primaryKey({ columns: [table.applicationId, table.gigId] }),
+	})
+);
 
+export const userProfile = onchainTable(
+	"userProfile",
+	(t) => ({
+		address: t.text().notNull(),
+		username: t.text(),
+		biography: t.text(),
+		email: t.text(),
+		profilePicture: t.text(),
+		bannerPicture: t.text(),
+		xUrl: t.text(),
+		instagramUrl: t.text(),
+		linkedinUrl: t.text(),
+		artstationUrl: t.text(),
+		sketchfabUrl: t.text(),
+		customUrl: t.text(),
+	}),
+	(table) => ({
+		pk: primaryKey({ columns: [table.address] }),
+	})
+);
---
+++ packages/ponder/src/UserProfile.ts
@@ -0,0 +1,20 @@
+import { ponder } from "ponder:registry";
+import { userProfile } from "ponder:schema";
+
+// ProfileUpdated event handler
+ponder.on(
+	"ProfileConfigContract:ProfileUpdated",
+	async ({ event, context }) => {
+		// Insert or update the user profile
+		console.log("ProfileUpdated event received:", event.args);
+		await context.db
+			.insert(userProfile)
+			.values({
+				address: event.args.user,
+				...event.args.profile,
+			})
+			.onConflictDoUpdate({
+				...event.args.profile,
+			});
+	}
+);
