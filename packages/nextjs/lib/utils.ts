9aa777a94ef2ac742e89e1a070b3aa4964723724
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-08-02 13:56:00 -0300

    feat: isImageUrl utils

--- packages/nextjs/lib/utils.ts
+++ packages/nextjs/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function isImageUrl(url: string): boolean {
+  try {
+    const pathname = new URL(url, "http://dummy-base").pathname;
+    return (
+      url.startsWith("http") || url.startsWith("data:image") || /\.(jpg|jpeg|png|gif|webp|svg)(\?.*)?$/.test(pathname)
+    );
+  } catch {
+    return false;
+  }
+}
