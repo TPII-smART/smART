5d8d031fd11a3c48c9fc03d7e2a9b60799e8fcde
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-08-09 22:42:46 -0300

    feat: list component

---
+++ packages/nextjs/components/List/List.tsx
@@ -0,0 +1,74 @@
+import { redirect } from "next/navigation";
+import { ListProps } from "./types";
+import {
+  Avatar as MUIAvatar,
+  List as MUIList,
+  ListItem as MUIListItem,
+  ListItemAvatar as MUIListItemAvatar,
+  ListItemButton as MUIListItemButton,
+  ListItemText as MUIListItemText,
+} from "@mui/material";
+import { blo } from "blo";
+
+const List = <T,>({ items, secondaryAction, paddingY = 4, descriptionLines = 3 }: ListProps<T>) => {
+  return (
+    <MUIList dense sx={{ width: "100%", bgcolor: "transparent" }}>
+      {items.map(i => {
+        return (
+          <MUIListItem
+            key={i.id}
+            secondaryAction={secondaryAction ? secondaryAction(i) : undefined}
+            sx={{
+              borderBottom: "1px solid var(--color-border)",
+              "& .MuiButtonBase-root": {
+                paddingY,
+              },
+              "& .MuiListItemSecondaryAction-root": {
+                width: "15%",
+              },
+            }}
+            disablePadding
+            slotProps={{
+              root: {
+                onClick: i.href ? () => redirect(i.href ?? "#") : i.onClick ? i.onClick : undefined,
+              },
+            }}
+          >
+            <MUIListItemButton>
+              <MUIListItemAvatar>
+                <MUIAvatar
+                  alt={`${i.id} avatar`}
+                  src={
+                    i.userProfilePictureURL ? i.userProfilePictureURL : i.userAddress ? blo(i.userAddress) : undefined
+                  }
+                />
+              </MUIListItemAvatar>
+              <MUIListItemText
+                id={i.id}
+                primary={<span className="text-primary-content">{i.title}</span>}
+                secondary={
+                  <span
+                    className="text-secondary-content"
+                    style={{
+                      overflowWrap: "break-word",
+                      wordBreak: "break-all",
+                      whiteSpace: "pre-line",
+                      display: "-webkit-box",
+                      WebkitLineClamp: descriptionLines,
+                      WebkitBoxOrient: "vertical",
+                      overflow: "hidden",
+                    }}
+                  >
+                    {i.description}
+                  </span>
+                }
+              />
+            </MUIListItemButton>
+          </MUIListItem>
+        );
+      })}
+    </MUIList>
+  );
+};
+
+export default List;
