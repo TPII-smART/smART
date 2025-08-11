28f5ff8231a5a6edfec31272b8eb5bbfeb08f0e6
Tinchocw <mcwikla@fi.uba.ar>
2025-08-11 20:11:12 -0300

    feat: add image config

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -14,6 +14,38 @@ const nextConfig: NextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  images: {
+    domains: [
+      "cdn.pixabay.com",
+      "efe8eb5ec0d79b037539c823b17ef9f0.ipfscdn.io",
+      "ipfs.io",
+      "gateway.pinata.cloud",
+      "cloudflare-ipfs.com",
+      "dweb.link",
+      "infura-ipfs.io",
+    ],
+    // O usar remotePatterns (más seguro y moderno)
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.ipfscdn.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ipfs.io",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+    ],
+  },
 };
 
 const isIpfs = process.env.NEXT_PUBLIC_IPFS_BUILD === "true";
