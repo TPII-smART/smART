4eca2befbe1421671c5a9fc793dbc036edc51e5f
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-08-12 21:54:18 -0300

    feat: add default expanded accordion

--- packages/nextjs/components/Accordion/Accordion.tsx
+++ packages/nextjs/components/Accordion/Accordion.tsx
@@ -6,25 +6,23 @@ import MUIAccordionDetails from "@mui/material/AccordionDetails";
 import MUIAccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 
-export default function Accordion(props: AccordionProps) {
+export default function Accordion({ title, children, defaultExpanded = true }: AccordionProps) {
   return (
     <MUIAccordion
       sx={{
         width: "100%",
         backgroundColor: "var(--color-surface)",
         "& .MuiSvgIcon-root": { color: "var(--color-primary-content)" },
+        borderBottom: "1px solid var(--color-border)",
       }}
+      defaultExpanded={defaultExpanded}
     >
-      <MUIAccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls={`${props.title}-content`}
-        id={`${props.title}-header`}
-      >
+      <MUIAccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`${title}-content`} id={`${title}-header`}>
         <Typography component="span" className="text-primary-content">
-          {props.title}
+          {title}
         </Typography>
       </MUIAccordionSummary>
-      <MUIAccordionDetails>{props.children}</MUIAccordionDetails>
+      <MUIAccordionDetails>{children}</MUIAccordionDetails>
     </MUIAccordion>
   );
 }
--- packages/nextjs/components/Accordion/types.ts
+++ packages/nextjs/components/Accordion/types.ts
@@ -1,4 +1,14 @@
 export interface AccordionProps {
+  /**
+   * The title of the accordion section.
+   */
   title: string;
+  /**
+   * The content of the accordion section.
+   */
   children: React.ReactNode;
+  /**
+   * Whether the accordion section is expanded by default.
+   */
+  defaultExpanded?: boolean;
 }
