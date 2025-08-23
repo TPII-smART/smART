a69ac81d0a9e13f04c97f63d3386e2c18ac6756b
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-08-22 23:25:16 -0300

    refactor: merge browsers

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -61,8 +61,8 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
               icon: WrenchScrewdriverIcon,
             },
             {
-              label: "Browse Jobs",
-              href: "/browse-jobs",
+              label: "Browse",
+              href: "/browse",
               icon: BookOpenIcon,
             },
             {
@@ -74,11 +74,6 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
               label: "My Applications",
               href: "/applications",
               icon: WrenchScrewdriverIcon,
-            },
-            {
-              label: "Browse Gigs",
-              href: "/browse-gigs",
-              icon: BookOpenIcon,
             },
             {
               label: "Ponder",
