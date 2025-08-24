d93be0d241a28438867e733035a7d5b7f800b528
Tinchocw <mcwikla@fi.uba.ar>
2025-08-24 20:38:25 -0300

    feat: Add type of event

--- packages/nextjs/types/feed/activityItem.type.ts
+++ packages/nextjs/types/feed/activityItem.type.ts
@@ -19,9 +19,16 @@ export enum ActivityItemStatus {
   unknown = "",
 }
 
+export enum InteractionType {
+  job = "job",
+  gig = "gig",
+  unknown = "",
+}
+
 export interface ActivityItem {
   id: string;
   type: ActivityItemType;
+  interactionType: InteractionType;
   title?: string;
   description?: string;
   timestamp: string;
