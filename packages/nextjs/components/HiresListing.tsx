00b5204658384546a5e3035e46c463a88daae907
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-09-22 13:48:31 -0300

    build: update deps

--- packages/nextjs/components/HiresListing.tsx
+++ packages/nextjs/components/HiresListing.tsx
@@ -35,7 +35,9 @@ export default function HiresListing({ userAddress }: { userAddress: string }) {
         <div className="w-full">
           {data?.jobs && data.jobs.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-              {data?.jobs.map(job => <JobCard key={`${job.jobId}-${job.postingId}`} job={job} reload={reload} />)}
+              {data?.jobs.map(job => (
+                <JobCard key={`${job.jobId}-${job.postingId}`} job={job} reload={reload} />
+              ))}
             </div>
           ) : (
             <div className="text-center py-12">
--- packages/nextjs/components/MyGigsListing.tsx
+++ packages/nextjs/components/MyGigsListing.tsx
@@ -35,7 +35,9 @@ export default function MyGigsListing({ userAddress }: { userAddress: string }) 
         <div className="w-full">
           {data?.gigs && data.gigs.length > 0 ? (
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-              {data?.gigs.map(gig => <GigCard key={gig.gigId} gig={gig} reload={reload} />)}
+              {data?.gigs.map(gig => (
+                <GigCard key={gig.gigId} gig={gig} reload={reload} />
+              ))}
             </div>
           ) : (
             <div className="text-center py-12">
